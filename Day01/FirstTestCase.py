594153614e1b76d11b58a57a774d1121e7d01a52
1008Alex <alexioanyt@gmail.com>
2023-03-01 21:15:12 +0530

    Day 1 Learn

--- Day01/FirstTestCase.py
+++ Day01/FirstTestCase.py
@@ -12,29 +12,33 @@ driver = webdriver.Chrome(service=service_obj)
 # 2) Open URL https://opensource-demo.orangehrmlive.com/
 
 driver.get("https://opensource-demo.orangehrmlive.com/")
-time.sleep(5)
+driver.implicitly_wait(5)
 # 3) Enter username (Admin).
-driver.find_element(By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/form[1]/div[1]/div[1]/div[2]/input[1]').click()
+driver.find_element(By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/form[1]/div[1]/div[1]/div[2]/input[1]').send_keys('Admin')
 
 # 4) Enter password (adnini23).
+driver.find_element(By.XPATH, "(//input[@placeholder='Password'])[1]").send_keys('admin123')
+time.sleep(5)
 
 # 5) Click on Login.
+
+driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
 
 # 6) Capture title of the home page.(Actual title)
 
-# act_title = driver.title
+act_title = driver.title
 
 # 7) Verify title of the page: OrangeHRM(Expected)
 #
-# exp_title = "OrangeHRM™
-#
-# if act_title == exp_title:
-#     print("Login Test Passed")
-# else:
-#     print("Login Test Passed")
+exp_title="OrangeHRM"
+
+if act_title == exp_title:
+    print("Login Test Passed")
+else:
+    print("Login Test Passed")
 
 # 8) close browser
 
 # driver.close()
-time.sleep(500)
+time.sleep(10)
